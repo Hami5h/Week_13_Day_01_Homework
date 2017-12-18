1dd5beb5a2412c9956930d8bedcf542b74274b42
Hamish <hamishfpso@gmail.com>
2017-12-18 16:29:46 +0000

    Initial commit. App is running

---
+++ controllers/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = new express.Router();
+
+const filmRouter = require('./films.js')
+router.use('/films', filmRouter);
+
+router.get('/', function (req, res) {
+  res.json({ data: 'Hello!' });
+});
+
+module.exports = router;
