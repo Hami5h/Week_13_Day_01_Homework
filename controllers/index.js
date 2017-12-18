657b176551aa62549dc0ed1d60a69ce8f38267c1
Hamish <hamishfpso@gmail.com>
2017-12-18 17:51:15 +0000

    crud actions completd

--- controllers/index.js
+++ controllers/index.js
@@ -4,8 +4,8 @@ const router = new express.Router();
 const filmRouter = require('./films.js')
 router.use('/films', filmRouter);
 
-router.get('/', function (req, res) {
-  res.json({ data: 'Hello!' });
-});
+// router.get('/', function(req, res){
+//   res.json({data: 'Hello'})
+// })
 
 module.exports = router;
